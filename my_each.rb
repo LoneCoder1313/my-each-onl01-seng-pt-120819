08d4413294b0dd8d408f837317bdff815bcb9398
Ricky dildy <rdildy92@gmail.com>
2019-12-22 22:01:23 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,9 @@
     words = ['hi', 'hello', 'bye', 'goodbye']
-def my_each(words) do |i|
-   puts i 
-end      
+def my_each(words) 
+   i=0 
+   while i < words.length 
+   yield(words[i])
+   i+=1 
+end
+   words 
 end
